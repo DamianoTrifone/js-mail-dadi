0d970bed6d11e76b2e06bc3f2c08fe9b70e28b1d
DamianoTrifone <trifone.damiano@gmail.com>
2021-04-28 16:30:52 +0200

    Numero random giocatore

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -3,8 +3,6 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 // Generare numero random giocatore
-function getRandomIntInclusive("1, 6") {
-    min = Math.ceil(1);
-    max = Math.floor(6);
-    return Math.floor(Math.random() * (max - min + 1)) + min; 
-  }
+var random = Math.floor(math.random() * 6) + 1;
+
+console.log(random);
