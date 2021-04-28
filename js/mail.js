9ef304774ea38ea4e25f0d977c8256acddd21337
DamianoTrifone <trifone.damiano@gmail.com>
2021-04-28 13:26:19 +0200

    Lista email autorizzate ed inserimento da parte dell'utente

--- js/mail.js
+++ js/mail.js
@@ -6,3 +6,15 @@
 // 2 - Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// Esercizio 1
+
+// Lista indirizzi email autorizzati
+var emailAutorizzate = ["pippo@tiscali.it", "paperino@tiscali.it", "gianni@yahoo.it", "damianotrifone@gmail.com",
+"booleancareers@info.it"];
+
+// Inserimento dell'indirizzo email da parte dell'utente
+var email = prompt("Inserisci qui il tuo indirizzo email");
+console.log(email);
+
+// Verifica autorizzazione email
